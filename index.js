99b908fc3416df590379377323492e19694ca227
Valentin Krasontovitsch <v.krasontov@gmail.com>
2017-01-25 21:07:30 +0100

    Format time

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/time/', (request, response) => {  
-  response.send(new Date())
+  let date = new Date()
+  let time = date.getHours() + ':'
+  time += date.getMinutes() + ':' + date.getSeconds()
+  response.send(time)
 })
 
 app.listen(port, (err) => {  
